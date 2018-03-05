4e8c8af17fd5bc550a57445adc46f0903b27a155
arthur <arthur.teisseire@epitech.eu>
2018-03-05 18:59:25 +0100

    my_strcmp now return 0

--- lib/my/my_strcmp.c
+++ lib/my/my_strcmp.c
@@ -12,11 +12,11 @@ int my_strcmp(char const *s1, char const *s2)
 	int i = 0;
 
 	if (s1 == NULL || s2 == NULL)
-		return (0);
+		return (-1);
 	while (s1[i] == s2[i]) {
 		if (s1[i] == '\0' && s2[i] == '\0')
-			return (1);
+			return (0);
 		i++;
 	}
-	return (0);
+	return (s1[i] - s2[i]);
 }
