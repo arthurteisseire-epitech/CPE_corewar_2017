fa9c7b339c27fed266f64bf1f610b25147a8b704
arthur <arthur.teisseire@epitech.eu>
2018-03-05 22:47:41 +0100

    [*]asm check now set_lines

--- asm/src/asm.c
+++ asm/src/asm.c
@@ -5,12 +5,24 @@
 ** by Arthur Teisseire
 */
 
+#include <stdlib.h>
+#include <fcntl.h>
 #include "my.h"
 #include "asm.h"
 
 int my_asm(buffer_t *buffer, char *pathname)
 {
-	my_putstr(pathname);
-	my_putchar('\n');
+	int fd = open(pathname, O_RDONLY);
+	char *line = get_next_line(fd);
+	int index = 0;
+
+	while (line != NULL) {
+		buffer->line = realloc(buffer->line, sizeof(line_t));
+		if (set_line(&buffer->line[index], line) == -1)
+			return (-1);
+		free(line);
+		line = get_next_line(fd);
+		index++;
+	}
 	return (0);
 }
