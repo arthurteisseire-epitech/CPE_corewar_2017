94913d74b76411e2359ba081116d8d402121ee90
Mahé Caurette <mahe.caurette@epitech.eu>
2018-03-06 18:05:12 +0100

    [+] new test in get_id_cmd.c -- testing is_opt_end

--- asm/tests/get_id_cmd_test.c
+++ asm/tests/get_id_cmd_test.c
@@ -10,24 +10,33 @@
 #include "asm.h"
 #include "parse.h"
 #include "op.h"
+#include <stdio.h>
 
 char const separators[3] = {SEPARATOR_CHAR, ' ', '\t'};
 
-Test(get_id_cmd, )
+Test(get_id_cmd, return_index)
 {
-	cr_expect(get_id_cmd(op_tab, "live") == 1);
-	cr_expect(get_id_cmd(op_tab, "ld") == 2);
-	cr_expect(get_id_cmd(op_tab, "st") == 3);
-	cr_expect(get_id_cmd(op_tab, "add") == 4);
-	cr_expect(get_id_cmd(op_tab, "sub") == 5);
-	cr_expect(get_id_cmd(op_tab, "and") == 6);
-	cr_expect(get_id_cmd(op_tab, "or") == 7);
-	cr_expect(get_id_cmd(op_tab, "xor") == 8);
+	cr_expect(get_id_cmd(op_tab, "unknown_cmd") == -1);
+	cr_expect(get_id_cmd(op_tab, "live") == 0);
+	cr_expect(get_id_cmd(op_tab, "ld") == 1);
+	cr_expect(get_id_cmd(op_tab, "st") == 2);
+	cr_expect(get_id_cmd(op_tab, "add") == 3);
+	cr_expect(get_id_cmd(op_tab, "sub") == 4);
+	cr_expect(get_id_cmd(op_tab, "and") == 5);
+	cr_expect(get_id_cmd(op_tab, "or") == 6);
+	cr_expect(get_id_cmd(op_tab, "xor") == 7);
+	cr_expect(get_id_cmd(op_tab, "zjmp") == 8);
 	cr_expect(get_id_cmd(op_tab, "ldi") == 9);
 	cr_expect(get_id_cmd(op_tab, "sti") == 10);
 	cr_expect(get_id_cmd(op_tab, "fork") == 11);
 	cr_expect(get_id_cmd(op_tab, "lld") == 12);
 	cr_expect(get_id_cmd(op_tab, "lldi") == 13);
-	cr_expect(get_id_cmd(op_tab, "lfor") == 14);
+	cr_expect(get_id_cmd(op_tab, "lfork") == 14);
 	cr_expect(get_id_cmd(op_tab, "aff") == 15);
+}
+
+Test(get_id_cmd, is_opt_end)
+{
+	cr_expect(is_opt_end(op_tab[0]) == -1);
+	cr_expect(is_opt_end(op_tab[16]) == 1);
 }
