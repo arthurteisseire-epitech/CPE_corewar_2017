8a9a49ab9520c395405d90291c040a8669d6f0b3
arthur <arthur.teisseire@epitech.eu>
2018-03-09 23:17:29 +0100

    [*]errors.h -- 'invalid label named' added

--- asm/include/errors.h
+++ asm/include/errors.h
@@ -31,6 +31,7 @@
 #define WRONG_NB_ARGS "Too many arguments given to the instruction."
 #define WRONG_REGISTER_NB "Invalid register number."
 #define WRONG_LABEL "Undefined label."
+#define INVALID_LABEL "Invalid label name."
 void put_or_init_err(char **av_init, int index_arg);
 void put_err_asm(char *error, int line);
 
