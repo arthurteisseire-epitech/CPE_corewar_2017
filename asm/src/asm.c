494ab68a5e9890c6766bd235ff0c1caa43a66b24
arthur <arthur.teisseire@epitech.eu>
2018-03-09 17:22:46 +0100

    Now asm is restructured need to fill functions on it

--- asm/src/asm.c
+++ asm/src/asm.c
@@ -5,52 +5,49 @@
 ** by Arthur Teisseire
 */
 
-#include <stdlib.h>
 #include <fcntl.h>
-#include "my.h"
-#include "op.h"
 #include "asm.h"
-#include "parse.h"
-#include "buffer.h"
-#include "line.h"
 
-int my_asm(buffer_t *buffer, char *pathname)
+/* TO DO : RETURN CASCADE */
+int my_asm(char *pathname)
 {
-	int fd = open(pathname, O_RDONLY);
-	int status = 0;
-	int index = 0;
+	buffer_t buffer;
 	header_t header;
+	int fd = open(pathname, O_RDONLY);
+	int status;
 
-	if (fd == -1)
-		return (-1);
-	init_buffer(buffer);
-	while (status != FEND && status != -1) {
-		status = store_and_check_line(fd, buffer, index, &header);
-		index++;
-	}
-	set_buffer_bytes(buffer);
-	//write(open("./test.cor", O_WRONLY), &header, sizeof(header_t));
+	set_header(&header, fd);
+	set_buffer(&buffer, fd);
+	status = set_binary(&buffer);
+	if (status == 0)
+		write_binary(&buffer);
+	destroy(&buffer);
 	return (status);
 }
 
-int store_and_check_line(int fd, buffer_t *buffer, int index, header_t *header)
+int set_header(header_t *header, int fd)
 {
-	char *line;
 
-	line = get_next_line(fd);
-	if (skip_comments(fd, &line, " \t") == FEND)
-		return (FEND);
-	buffer->lines = realloc(buffer->lines, sizeof(line_t) * (index + 1));
-	if (buffer->lines == NULL)
-		return (-1);
-	buffer->lines[index].index = index;
-	if (set_line(buffer, &buffer->lines[index], line) == -1)
-		return (-1);
-	if (index < 2)
-		if (set_header(&buffer->lines[index], header) == -1)
-			my_putstr("connard\n");
-	buffer->nb_lines += 1;
-	if (line != NULL)
-		free(line);
-	return (0);
+}
+
+int set_buffer(buffer_t *buffer, int fd)
+{
+	char *line = get_next_line(fd);
+
+	skip_comments_and_labels(buffer, &line, fd);
+	set_line(buffer, line);
+}
+
+int set_binary(buffer_t *buffer)
+{
+	for (int i = 0; i < buffer->nb_lines; i++) {
+		replace_label_call(buffer, &buffer->line[i]);
+		is_line_valide(&buffer->line[i]);
+		set_binary_line(buffer, &buffer->line[i]);
+	}
+}
+
+int write_binary(buffer_t *buffer)
+{
+
 }
---
+++ asm/src/parse1.c
@@ -0,0 +1,56 @@
+/*
+** EPITECH PROJECT, 2017
+** File Name : asm.c
+** File description:
+** by Arthur Teisseire
+*/
+
+#include <stdlib.h>
+#include <fcntl.h>
+#include "my.h"
+#include "op.h"
+#include "asm.h"
+#include "parse.h"
+#include "buffer.h"
+#include "line.h"
+
+int my_asm(buffer_t *buffer, char *pathname)
+{
+	int fd = open(pathname, O_RDONLY);
+	int status = 0;
+	int index = 0;
+	header_t header;
+
+	if (fd == -1)
+		return (-1);
+	init_buffer(buffer);
+	while (status != FEND && status != -1) {
+		status = store_and_check_line(fd, buffer, index, &header);
+		index++;
+	}
+	set_buffer_bytes(buffer);
+	//write(open("./test.cor", O_WRONLY), &header, sizeof(header_t));
+	return (status);
+}
+
+int store_and_check_line(int fd, buffer_t *buffer, int index, header_t *header)
+{
+	char *line;
+
+	line = get_next_line(fd);
+	if (skip_comments(fd, &line, " \t") == FEND)
+		return (FEND);
+	buffer->lines = realloc(buffer->lines, sizeof(line_t) * (index + 1));
+	if (buffer->lines == NULL)
+		return (-1);
+	buffer->lines[index].index = index;
+	if (set_line(buffer, &buffer->lines[index], line) == -1)
+		return (-1);
+	if (index < 2)
+		if (set_header(&buffer->lines[index], header) == -1)
+			my_putstr("connard\n");
+	buffer->nb_lines += 1;
+	if (line != NULL)
+		free(line);
+	return (0);
+}
